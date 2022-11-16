72ef6dc9e894789a4459f5508ed8b3d123e6ccca
Soumya78@ <sr2601335@gmail.com>
2022-11-16 21:02:30 +0530

    newchanges

--- lib/widgets/orderitem.dart
+++ lib/widgets/orderitem.dart
@@ -1,17 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart' ;
 import 'package:intl/intl.dart';
 
 import 'package:shopp/providers/order.dart' as ord;
-class Orderitem extends StatelessWidget {
+class Orderitem extends StatefulWidget {
 
 final ord.Orderitem orde ;
     Orderitem(this.orde);
-  
 
-  
+  @override
+  State<Orderitem> createState() => _OrderitemState();
+}
 
-  
-
+class _OrderitemState extends State<Orderitem> {
+  var _expanded = false ;
   @override
   Widget build(BuildContext context) {
     
@@ -19,10 +22,25 @@ final ord.Orderitem orde ;
     return Card(
        margin: EdgeInsets.all(10.0),
        child: Column(children: <Widget>[
-        ListTile(title: Text('\$${orde.amount}'),
-        subtitle: Text(DateFormat('dd MM yyyy hh:mm ').format(orde.datetime),),
-        trailing: IconButton(icon: Icon(Icons.expand_more),onPressed: (){},),)
-       ],),
+        ListTile(title: Text('\$${widget.orde.amount}'),
+        subtitle: Text(DateFormat('dd MM yyyy hh:mm ').format(widget.orde.datetime),),
+        trailing: IconButton(icon: Icon(_expanded ? Icons.expand_less:Icons.expand_more),
+        onPressed: (){
+           setState(() {
+             _expanded = !_expanded ;
+           },
+           );
+ 
+        },
+        ),
+        ),
+        if(_expanded) Container(height: min(widget.orde.products!.length * 20.0 + 100 , 180),
+        child: ListView(
+        
+        ),
+       ),
+       ],
+       ),
     );
   }
 }
