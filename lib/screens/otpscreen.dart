2da1f5d2dca0a2f7ca8ee1044e2c9a9ea24e1ff3
Soumya78@ <sr2601335@gmail.com>
2022-12-21 08:16:46 +0530

    changes

--- lib/screens/otpscreen.dart
+++ lib/screens/otpscreen.dart
@@ -5,35 +5,63 @@ import 'package:http/http.dart'as http;
 import 'dart:convert';
 
 import 'package:shopp/screens/products_overview_screens.dart';
-class OtpScreen extends StatelessWidget {
+import 'package:sms_autofill/sms_autofill.dart';
+class OtpScreen extends StatefulWidget {
  // const OtpScreen({ Key? key }) : super(key: key);
   static const routenamed = '/Otpscreen';
-  TextEditingController otpcontroller = new TextEditingController();
 
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> with CodeAutoFill {
+  String codevalue = "";
+  
+  @override
+  void initState(){
+    super.initState();
+    _listenotp();
+  }
+  TextEditingController otpcontroller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     
-    final otp = Provider.of<Otpitems>(context);
+   //final otp = Provider.of<Otpitems>(context);
     return Scaffold(appBar: AppBar(title:Text("OtpScreen"),)
-    ,body: Container(child: Column(children: <Widget>[
-      TextField(decoration: InputDecoration(),onChanged: (value){
-         otpcontroller.text = value;
-      },),
-      ElevatedButton(onPressed: () async{
-        //await otp.adddata(otpcontroller.text);
-        await otp.adddata(otpcontroller.text.toString());
-        Navigator.of(context).pushNamed(ProductsOverviewScreen.routenamed);
+    ,body: Center(child: Column(mainAxisAlignment: MainAxisAlignment.center
+    ,children:<Widget> [
+       Container(padding: EdgeInsets.symmetric(horizontal: 50,vertical: 10),
+       child: PinFieldAutoFill(currentCode: code,codeLength: 4,onCodeChanged:(val){
+          _listenotp();
+       },),),
+       ElevatedButton(onPressed: () async{
+        await  SmsAutoFill().listenForCode();
+       }, child: Text("Listen for sms code")),
+    ],
+    ),),
+    );
         
         // var url = Uri.parse('https://shopapp-14acd-default-rtdb.firebaseio.com/credentials.json');
         // http.post(url,body:json.encode({'otp':otpcontroller.text.toString()}));
          
-      }, child: Text('Save the otp'))
+    
        
          
-    ],
-    ),
-    ),
-    );
-  }
+  
+
+}
+
+  @override
+  void codeUpdated() {
+    print("Update code $code");
+    setState(() {
+      print("codeupdated");
+    });
+    // TODO: implement codeUpdated
+  }}
+
+void _listenotp() async {
+ await SmsAutoFill().listenForCode;
+ print("otp listen called");
 }
--- lib/screens/registrationscreen.dart
+++ lib/screens/registrationscreen.dart
@@ -1,77 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shopp/screens/otpscreen.dart';
+import 'package:sms_autofill/sms_autofill.dart';
 import 'package:twilio_flutter/twilio_flutter.dart';
 
 
 class RegistrationScreen extends StatefulWidget {
   static const routenamed = '/RegistrationScreen';
-  const RegistrationScreen({ Key? key }) : super(key: key);
 
   @override
   State<RegistrationScreen> createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-  late TwilioFlutter twilio_flutter ;
-  
-  TextEditingController ?countrycode = new TextEditingController();
-  var phone ="";
-  @override
-  void initState(){
-    //print('its working 1');
-    twilio_flutter = TwilioFlutter(accountSid: 'ACcb8811e5328cc238f2977354b9d151a4', 
-    authToken: '79c7a131fe5833d04577362b51c0e402', twilioNumber: '+91 934848 6728');
-   // countrycode!.text = "+91";
+ //const RegistrationScreen({ Key? key }) : super(key: key);
+  TextEditingController ?mobileno = new TextEditingController();
 
-    super.initState();
-  }
-  void sendsms (String number) async{
-    twilio_flutter.sendSMS(toNumber: number, messageBody: 'hi this is your code');
-  }
+ void sumbitotp() async{
+    final signcode = await SmsAutoFill().getAppSignature;
+    Map sendOtpdata = {
+      'mobile-no': mobileno!.text.toString() ,
+      'otp-signature': signcode.toString()
+    };
+
+              if(!signcode.isEmpty ){
+                Navigator.of(context).pushNamed(OtpScreen.routenamed);
+              }
+              print(sendOtpdata.entries);
+ }
+
+  var phone ="";
+
+ // void sendsms (String number) async{
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(title: Text("Registration Screen"),),
-
-      body: Container(child: Column(children: <Widget>[
-            SizedBox(height:600,child: Container(child: Column(children: <Widget>[
+      
+      body: Container(width: double.infinity,
+      height: MediaQuery.of(context).size.height,
+      child: Column(children: <Widget>[
+            Expanded(
+              child:  Container(child: Column(mainAxisAlignment: MainAxisAlignment.center,children: <Widget>[
+                
               
+                 Image.network('https://cdn.freebiesupply.com/logos/thumbs/2x/firebase-1-logo.png',
+                  width: double.infinity,),
+                  Expanded(child: Text("Mobile Number",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)),
+                  Expanded(child: Text("Enter your mobile number to continue")),
+              ],),color: Colors.yellow),
+              ),
             
-               Image.network('https://cdn.freebiesupply.com/logos/thumbs/2x/firebase-1-logo.png',
-                height: 500,width: double.infinity,),
-                Text("Mobile Number",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                Text("Enter your mobile number to continue"),
-            ],),color: Colors.yellow),),
           
           SizedBox(height:10),
-        Padding(
-          padding: EdgeInsets.all(10),
-          child: TextField(onChanged: (value){
-            phone=value ;
-          },keyboardType: TextInputType.phone,
-          decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-          labelText: 'Enter your phone number',
-          contentPadding: EdgeInsets.all(10)),controller: countrycode,),
-        ),
-        SizedBox(
-          child: ElevatedButton(style: ButtonStyle(),onPressed: ()async {
-            print('${phone}');
-            sendsms(phone);
-
-         /*  await FirebaseAuth.instance.verifyPhoneNumber(
-            
-          phoneNumber:'${phone}',
-          verificationCompleted: (PhoneAuthCredential credential) {
-         
-          },
-          verificationFailed: (FirebaseAuthException e) {print(e.message);},
-          codeSent: (String verificationId, int? resendToken) { Navigator.pushNamed(context,OtpScreen.routenamed);},
-          codeAutoRetrievalTimeout: (String verificationId) {},
-        ); */
-          }, child: Text('Get otp',style: TextStyle(fontSize: 20),),
+      
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(onChanged: (value){
+              phone=value ;
+            },keyboardType: TextInputType.phone,
+            decoration: InputDecoration(prefixIcon: Icon(Icons.call),border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            labelText: 'Enter your phone number',
+            contentPadding: EdgeInsets.all(10)),controller: mobileno,),
           ),
-        ),
+      
+         
+           ElevatedButton(style: ButtonStyle(),onPressed: ()async {
+            sumbitotp();
+            //  print('${phone}');
+             // sendsms(phone);
+         
+           /*  await FirebaseAuth.instance.verifyPhoneNumber(
+              
+            phoneNumber:'${phone}',
+            verificationCompleted: (PhoneAuthCredential credential) {
+           
+            },
+            verificationFailed: (FirebaseAuthException e) {print(e.message);},
+            codeSent: (String verificationId, int? resendToken) { Navigator.pushNamed(context,OtpScreen.routenamed);},
+            codeAutoRetrievalTimeout: (String verificationId) {},
+                 ); */
+            }, child: Text('Get otp',style: TextStyle(fontSize: 20),),
+            ),
+         
+        
 
       ],
       ),
